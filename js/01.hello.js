298914c571ed8198a9033b6db9c8c380daec7c9d
zhangshicong <18339819268@163.com>
2022-11-15 17:57:35 +0800

    jquery插件、mongodb插件提交

---
+++ js/01.hello.js
@@ -0,0 +1,17 @@
+function fn() {
+    console.log('ss');
+};
+
+fn();
+
+var a = 1;
+console.log(a);
+
+
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+};
+
+if (true) {
+    console.log(a);
+}
